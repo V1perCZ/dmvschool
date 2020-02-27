388c0e5b3fb46ab695c256972f76ed4e4f644d1d
Lukáš Müller <61428713+V1perCZ@users.noreply.github.com>
2020-02-27 19:27:07 +0100

    Add files via upload

---
+++ esx_dmvschool-master/html/questions_en.js
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+	{
+		question: "Pokud jedete rychlosti 80 km/h a priblizujete se k obytne oblasti, musite:",
+		propositionA: "Zrychlite",
+		propositionB: "Udrzujete svou rychlost 80km/h",
+		propositionC: "Zpomalis",
+		propositionD: "Zacnu vybrzdovat",
+		reponse: "C"
+	},
+
+	{
+		question: "Pokud odbocite doprava na semaforu, ale vidite prechod pro chodce, co udelate:",
+		propositionA: "Projedete kolem chodce",
+		propositionB: "Zajedu ho",
+		propositionC: "Pockam dokud chodec neprejde",
+		propositionD: "Zastrelim chodce a potom ho prejedu",
+		reponse: "C"
+	},
+
+	{
+		question: "Jaka je rychlost v obytne oblasti",
+		propositionA: "30 km/h",
+		propositionB: "50 km/h",
+		propositionC: "40 km/h",
+		propositionD: "60 km/h",
+		reponse: "B"
+	},
+
+	{
+		question: "Pred každou zmenou jizdniho pruhu musite:",
+		propositionA: "Zkontrolovat zrcatka",
+		propositionB: "Zkontrolovat cerne body",
+		propositionC: "Dat blinkr",
+		propositionD: "Vsechno z uvedeneho",
+		reponse: "D"
+	},
+
+	{
+		question: "Kolik % alkoholu muzete mit v krvi",
+		propositionA: "0.05%",
+		propositionB: "0.18%",
+		propositionC: "0.08%",
+		propositionD: "0.06%",
+		reponse: "C"
+	},
+
+	{
+		question: "Kdy můžete pokračovat v jízdě na semaforu?",
+		propositionA: "Kdyz je zelena",
+		propositionB: "Kdyz se stane bouracka",
+		propositionC: "Kdyz je cervena",
+		propositionD: "Kdyz je zelena a nebo cervena a odbocite doprava",
+		reponse: "D"
+	},
+
+	{
+		question: "Chodec ma nekrizeny signal, co delate?",
+		propositionA: "Nechal jsi je projit",
+		propositionB: "Pozorujete, nez budete pokracovat",
+		propositionC: "Prejedu ho",
+		propositionD: "Pokracujete, protoze vas semafor je zeleny",
+		reponse: "D"
+	},
+
+	{
+		question: "Co je povoleno pri predjizdeni vozidla",
+		propositionA: "Peclive ho sleduji, abych ho predjel rychleji",
+		propositionB: "Projdete kolem, aniz byste opustili vozovku",
+		propositionC: "Projedete na opacne strane silnice",
+		propositionD: "Prekrocite rychlostni limit, abyste ho mohli predjet",
+		reponse: "C"
+	},
+
+	{
+		question: "Jdete po dalnici, ktera ukazuje maximalni rychlost 120 km/h. Ale vetsina dopravcu jezdi rychlosti 125 km / h, takze byste nemeli jet rychleji nez:",
+		propositionA: "120 km/h",
+		propositionB: "125 km/h",
+		propositionC: "130 km/h",
+		propositionD: "110 km/h",
+		reponse: "A"
+	},
+
+	{
+		question: "Pokud vas predjizdi jine vozidlo, je dulezite:",
+		propositionA: "Zpomalit",
+		propositionB: "Zkontroluji zrcatka",
+		propositionC: "Kouknu se na ostatni ridice",
+		propositionD: "Zvyste svou rychlost",
+		reponse: "A"
+	},
+]
